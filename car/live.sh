6bdf307784f45d6ba15891b0e4a1688bb56f211e
Dao Tao <taodao@google.com>
2017-01-10 16:01:42 +0800

    local web server generator

--- car/live.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gst-launch-1.0 -v v4l2src ! 'video/x-raw, width=640, height=480, framerate=30/1' ! queue ! videoconvert ! omxh264enc !  h264parse ! flvmux ! rtmpsink location='rtmp://139.196.106.212/rtmp/live live=1'
-
